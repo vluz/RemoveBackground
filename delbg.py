8eeeb3ee08bb449e378b81f45c8442725ed2bd0a
vluz <vluz@users.noreply.github.com>
2023-06-11 22:32:18 +0100

    Add files via upload

---
+++ delbg.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import time
+import warnings
+import torch        # Don't ask
+import onnxruntime  # Won't tell
+from rembg import remove
+from PIL import Image
+
+
+warnings.filterwarnings("ignore")
+st.title("Remove Background")
+st.divider()
+uploaded_file = st.file_uploader("Upload a png file to remove background", type="png")
+
+if uploaded_file:
+    st.image(uploaded_file, caption='Original')
+    if st.button("Remove"):
+        with st.spinner("Removing..."):
+            input = Image.open(uploaded_file)
+            output = remove(input)
+            now = str(time.strftime("%Y%m%d-%H%M%S"))
+            filename = now + ".png"
+            output.save(filename)
+            st.image(output, caption='Result')
+
